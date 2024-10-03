711f5b3a42b1f437b99c4125eb22102478837987
manishaac2002 <manishaac2002@gmail.com>
2024-10-03 16:24:25 +0530

    feat - chore --

--- src/App.js
+++ src/App.js
@@ -167,9 +167,3 @@ export default function App() {
   )
 }
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/manishaac2002/usePopcorn-react.git
-// git push -u origin main
