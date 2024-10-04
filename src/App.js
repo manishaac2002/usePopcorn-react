8c33ae2ccc8f5fdceb55061f553cf53d945b4152
manishaac2002 <manishaac2002@gmail.com>
2024-10-04 15:59:52 +0530

    feat- props drilling..

--- src/App.js
+++ src/App.js
@@ -52,22 +52,23 @@ const average = (arr) =>
 
 export default function App() {
 
+  const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <Navbar />
-      <Main />
+      <Navbar movies={movies}/>
+      <Main movies={movies}/>
     </>
   )
 }
 
-function Navbar() {
+function Navbar({movies}) {
 
   const [query, setQuery] = useState("");
 
   return (<nav className="nav-bar">
     <Logo />
     <Search />
-    <NumResults />
+    <NumResults movies={movies} />
 
   </nav>)
 }
@@ -96,25 +97,25 @@ function Search() {
 
 }
 
-function NumResults() {
+function NumResults({movies}) {
   return (<p className="num-results">
-    Found <strong>X</strong> results
+    Found <strong>{movies.length}</strong> results
   </p>)
 }
 
-function Main() {
+function Main({movies}) {
 
   return (
 
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies} />
       <WatchedBox />
     </main>
 
   )
 }
 
-function ListBox() {
+function ListBox({movies}) {
 
   const [isOpen1, setIsOpen1] = useState(true);
 
@@ -127,15 +128,13 @@ function ListBox() {
         {isOpen1 ? "–" : "+"}
       </button>
       {isOpen1 && (
-        <MovieList />
+        <MovieList movies={movies}/>
       )}
     </div>
   )
 }
 
-function MovieList() {
-
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({movies}) {
 
   return (
     <ul className="list">
