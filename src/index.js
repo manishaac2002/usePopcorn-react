d479f52e38b1954df762c39e33e52c6590c78e4c
manishaac2002 <manishaac2002@gmail.com>
2024-10-22 17:56:16 +0530

    chore-

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import StarRating from './StarRating';
+// // import './index.css';
+// // import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     <StarRating maxRating={5} 
+//     message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+//     <StarRating maxRating={5} className="test" />
+//   </React.StrictMode>
+// );
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} 
-    message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
-    <StarRating maxRating={5} className="test" />
+    <App />
   </React.StrictMode>
 );
 
